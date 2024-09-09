ce716bbf44ae42cc657c0f659b399d4c8d0da341
gabriell3103 <g5819853@gmail.com>
2024-09-09 16:10:44 -0300

    Integrando e configurando o React-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+// Pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
 function App() {
 
   return (
-    <>
-      <h1>MiniBlog</h1>
-    </>
+    <div className='App'>
+      <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about'element={<About/>}></Route>
+        </Routes>
+      </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
