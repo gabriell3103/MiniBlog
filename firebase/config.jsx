7b24143c347608d0190bc3fe2a24e96daf71ae40
gabriell3103 <g5819853@gmail.com>
2024-09-09 15:58:49 -0300

    Integrando o Firebase

---
+++ firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnTMiOme52ImvLLwyvguL7_liEHrd02Kc",
+  authDomain: "miniblog-a73c5.firebaseapp.com",
+  projectId: "miniblog-a73c5",
+  storageBucket: "miniblog-a73c5.appspot.com",
+  messagingSenderId: "929063600350",
+  appId: "1:929063600350:web:a271a59f7048e2dba4c24f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      
+      <h1>MiniBlog</h1>
     </>
   )
 }
