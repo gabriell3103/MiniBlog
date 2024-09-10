e216d129be702226c75966a6c73900fdbff1be4d
gabriell3103 <g5819853@gmail.com>
2024-09-10 15:58:54 -0300

    implementação do about

--- src/App.jsx
+++ src/App.jsx
@@ -49,10 +49,10 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/about'element={<About/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/register'element={<Register/>}></Route>
-            <Route path='/posts/create'element={<CreatePost/>}></Route>
-            <Route path='/dashboard'element={<Dashboard/>}></Route>
+            <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}></Route>
+            <Route path='/register'element={!user ? <Register/> : <Navigate to="/"/>}></Route>
+            <Route path='/posts/create'element={user ? <CreatePost/> : <Navigate to="/login"/>}></Route>
+            <Route path='/dashboard'element={user ? <Dashboard/> : <Navigate to="/login"/>}></Route>
           </Routes>
         </div>
         <Footer />
