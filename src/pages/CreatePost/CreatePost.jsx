96a853338eb96628da2266c6349d36fa272a5fc8
gabriell3103 <g5819853@gmail.com>
2024-09-16 15:27:13 -0300

    Criação de posts e inserção dos dados no banco

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,8 +1,9 @@
-import styles from "./CreatePost.module.css"
+import styles from "./CreatePost.module.css";
 
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useAuthValue } from '../../context/AuthContext'
+import { useState } from "react";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from '../../context/AuthContext';
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -10,16 +11,63 @@ const CreatePost = () => {
   const [body, setBody] = useState("");
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
-  
+
+  const { user } = useAuthValue();
+
+  const navigate = useNavigate();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+    setFormError("");
+
+    // validate image
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    // create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    // check values
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    console.log(tagsArray);
+
+    console.log({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    if(formError) return
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
+    navigate("/");
+  };
 
   return (
     <div className={styles.create_post}>
-        <h2>Criar post</h2>
-        <p>Escreva sobre o que quiser falar e compartilhe seu conhecimento!</p>
-        <form onSubmit={handleSubmit}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Título:</span>
           <input
@@ -63,8 +111,7 @@ const CreatePost = () => {
             value={tags}
           />
         </label>
-        <button className="btn">Criar post!</button>
-        {/* {!response.loading && <button className="btn">Criar post!</button>}
+        {!response.loading && <button className="btn">Criar post!</button>}
         {response.loading && (
           <button className="btn" disabled>
             Aguarde.. .
@@ -72,10 +119,10 @@ const CreatePost = () => {
         )}
         {(response.error || formError) && (
           <p className="error">{response.error || formError}</p>
-        )} */}
-        </form>
+        )}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
