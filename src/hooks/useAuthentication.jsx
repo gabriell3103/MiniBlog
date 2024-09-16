150769dabf4eed1b5057f5668875b032df95f50c
gabriell3103 <g5819853@gmail.com>
2024-09-16 15:36:10 -0300

    Correção de mesnagem de  erro ao realizar o login

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -82,24 +82,22 @@ export const useAuthentication = () => {
     setError(false);
 
     try {
-        await signInWithEmailAndPassword(auth, data.email, data.password);
-        setLoading(false)
-      } catch (error) {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+      console.log(error.message.includes("user-not"));
 
-        let systemErrorMessage;
-  
-        if (error.message.includes("user-not-found")) {
-          systemErrorMessage = "Usuário não encontrado.";
-        } else if (error.message.includes("wrong-password")) {
-          systemErrorMessage = "Senha incorreta.";
-        } else {
-          systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
-        }
-  
-  
-        setError(systemErrorMessage);
-        setLoading(false)
+      let systemErrorMessage;
+
+      if (error.message.includes("invalid-credential")) {
+        systemErrorMessage = "Usuário ou senha inválido(a)";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
       }
+
+      setError(systemErrorMessage);
+    }
 
     setLoading(false);
   };
