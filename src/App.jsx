2b407accd67b5d143eab4da1ab0504c986eb282b
gabriell3103 <g5819853@gmail.com>
2024-09-17 14:34:24 -0300

    exibindo os posts individuais com o botão "Ler"

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Search from "./pages/Search/Search"
+import Post from './pages/Post/Post';
 
 function App() {
 
@@ -51,6 +52,7 @@ function App() {
             <Route path='/' element={<Home/>}></Route>
             <Route path='/about'element={<About/>}></Route>
             <Route path='/search'element={<Search/>}></Route>
+            <Route path='/posts/:id' element={<Post/>}></Route>
             <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}></Route>
             <Route path='/register'element={!user ? <Register/> : <Navigate to="/"/>}></Route>
             <Route path='/posts/create'element={user ? <CreatePost/> : <Navigate to="/login"/>}></Route>
