859a277e3b93fc093a2e0e29acac36f05bf827eb
gabriell3103 <g5819853@gmail.com>
2024-09-18 00:10:46 -0300

    Implementando a edição de posts

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Search from "./pages/Search/Search"
 import Post from './pages/Post/Post';
+import EditPost from './pages/EditPost/EditPost';
 
 function App() {
 
@@ -56,6 +57,7 @@ function App() {
             <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}></Route>
             <Route path='/register'element={!user ? <Register/> : <Navigate to="/"/>}></Route>
             <Route path='/posts/create'element={user ? <CreatePost/> : <Navigate to="/login"/>}></Route>
+            <Route path='/posts/edit/:id'element={user ? <EditPost/> : <Navigate to="/login"/>}></Route>
             <Route path='/dashboard'element={user ? <Dashboard/> : <Navigate to="/login"/>}></Route>
           </Routes>
         </div>
